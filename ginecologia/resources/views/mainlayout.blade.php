a4a001e46d8674b7cc53651d6bfc939a5f3be1d1
Alex27136 <60789023+Alex27136@users.noreply.github.com>
2022-05-06 16:44:52 -0500

    links

--- ginecologia/resources/views/mainlayout.blade.php
+++ ginecologia/resources/views/mainlayout.blade.php
@@ -14,7 +14,7 @@
     
 
     <!-- Bootstrap core CSS -->
-    <link href="file:///c:/xampp/htdocs/ginecologia/assets/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="file:///C:\Users\Ivan.LAPTOP-RB1MTRQK\Documents\GitHub\ginecologia\ginecologia\assets\dist\css\bootstrap.min.css" rel="stylesheet"/>
 
     <style>
       .bd-placeholder-img {
@@ -34,7 +34,7 @@
 
     
     <!-- Custom styles for this template -->
-    <link href="file:///c:/xampp/htdocs/ginecologia/resources/views/dashboard.css" rel="stylesheet">
+    <link href="file:///C:\Users\Ivan.LAPTOP-RB1MTRQK\Documents\GitHub\ginecologia\ginecologia\resources\views\dashboard.css" rel="stylesheet">
   </head>
   <body>
     
@@ -279,11 +279,11 @@
 </div>
 
 
-    <script src="file:///c:/xampp/htdocs/ginecologia/assets/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="file:///C:\Users\Ivan.LAPTOP-RB1MTRQK\Documents\GitHub\ginecologia\ginecologia\assets\dist\js\bootstrap.bundle.min.js"></script>
 
       <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous">
 
       </script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
-      <script src="file:///c:/xampp/htdocs/ginecologia/assets/dist/js/dashboard.js"></script>
+      <script src="file:///C:\Users\Ivan.LAPTOP-RB1MTRQK\Documents\GitHub\ginecologia\ginecologia\assets\dist\js\dashboard.js"></script>
   </body>
 </html>
